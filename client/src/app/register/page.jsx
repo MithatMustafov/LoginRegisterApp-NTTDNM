553be2581a2ce2153fd4584d5a615732bf230418
MithatMustafov <mithat.mustafow@gmail.com>
2024-08-06 21:18:51 +0300

    fix folders

---
+++ client/src/app/register/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+import RegisterPage from '../../appComponents/Pages/pageRegister/pageRegister';
+
+const PageRegister = () => {
+  return(
+    <div>
+      <RegisterPage/>
+    </div>
+  );
+};
+
+export default PageRegister;
