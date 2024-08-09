303a648509828b8d3b6ff5973949d033af752885
MithatMustafov <mithat.mustafow@gmail.com>
2024-08-09 22:12:55 +0300

    server side api rework

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca6a37b7a73a48b2b69883a904c585ee64cb9ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7881629babc74850176e5c484ce05de5efb42f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
